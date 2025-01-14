ea82b8d37f182a6071c123107ca91bef94915297
felipeErnica <felipe.ernica@gmail.com>
2025-01-14 15:34:46 -0300

    Teste classificacao

--- src/main/java/com/santacarolina/financeiro/repository/CategoriaRepository.java
+++ src/main/java/com/santacarolina/financeiro/repository/CategoriaRepository.java
@@ -3,12 +3,14 @@ package com.santacarolina.financeiro.repository;
 import java.util.Optional;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
 import com.santacarolina.financeiro.entity.CategoriaEntity;
 
 /**
  * CategoriaRepository
  */
+@Repository
 public interface CategoriaRepository extends JpaRepository<CategoriaEntity, Long> {
     Optional<CategoriaEntity> findByNome(String nome);
     Optional<CategoriaEntity> findByNumeroCategoria(String numeroCategoria);
--- src/main/java/com/santacarolina/financeiro/repository/ClassificacaoRepository.java
+++ src/main/java/com/santacarolina/financeiro/repository/ClassificacaoRepository.java
@@ -3,12 +3,14 @@ package com.santacarolina.financeiro.repository;
 import java.util.Optional;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
 import com.santacarolina.financeiro.entity.ClassificacaoEntity;
 
 /**
  * ClassificacaoRepository
  */
+@Repository
 public interface ClassificacaoRepository extends JpaRepository<ClassificacaoEntity, Long> {
     Optional<ClassificacaoEntity> findByNumeroIdentificacao(String numeroIdentificacao);
     Optional<ClassificacaoEntity> findByNomeClassificacao(String nomeClassificacao);
