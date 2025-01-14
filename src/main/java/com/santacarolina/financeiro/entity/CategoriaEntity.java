d444dc425ad6dfa394ef69686bf049c0494adf17
felipeErnica <felipe.ernica@gmail.com>
2025-01-14 15:11:34 -0300

    Teste classificacao

--- src/main/java/com/santacarolina/financeiro/entity/CategoriaEntity.java
+++ src/main/java/com/santacarolina/financeiro/entity/CategoriaEntity.java
@@ -19,7 +19,7 @@ import jakarta.persistence.Table;
 public class CategoriaEntity {
 
     @Id
-    @GeneratedValue(strategy = GenerationType.AUTO)
+    @GeneratedValue(strategy = GenerationType.SEQUENCE)
     private long id;
 
     @Enumerated(EnumType.ORDINAL)
