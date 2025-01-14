8ffec0220840ec20997d663fb5df1b85c603f4e6
felipeErnica <felipe.ernica@gmail.com>
2025-01-14 16:24:50 -0300

    Teste classificacao

--- src/main/java/com/santacarolina/financeiro/dao/CategoriaDAO.java
+++ src/main/java/com/santacarolina/financeiro/dao/CategoriaDAO.java
@@ -7,7 +7,6 @@ import java.util.List;
 import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.stereotype.Component;
 
 import com.santacarolina.financeiro.dto.CategoriaDTO;
 import com.santacarolina.financeiro.interfaces.DAO;
