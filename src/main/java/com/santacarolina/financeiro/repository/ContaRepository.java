8fc0197d22ad9f4228bd59e9843fa9999baa8dca
felipeErnica <felipe.ernica@gmail.com>
2025-01-15 11:31:00 -0300

    Refatoracao ContaController, ContaService, ContaDTO

--- src/main/java/com/santacarolina/financeiro/repository/ContaRepository.java
+++ src/main/java/com/santacarolina/financeiro/repository/ContaRepository.java
@@ -4,9 +4,9 @@ import java.util.Optional;
 
 import org.springframework.data.jdbc.repository.query.Query;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
-import com.santacarolina.financeiro.dto.ContaDTO;
 import com.santacarolina.financeiro.entity.ContaEntity;
 
 /**
@@ -20,5 +20,7 @@ public interface ContaRepository extends JpaRepository<ContaEntity, Long> {
         FROM ContaEntity c
         WHERE c.agencia = :agencia AND c.numeroConta = :numeroConta AND c.banco.id = :bancoId; 
         """)
-    Optional<ContaEntity> findEqual(String agencia, String numeroConta, long bancoId);
+    Optional<ContaEntity> findEqual(@Param("agencia") String agencia, 
+            @Param("numeroConta") String numeroConta, 
+            @Param("bancoId") long bancoId);
 }
