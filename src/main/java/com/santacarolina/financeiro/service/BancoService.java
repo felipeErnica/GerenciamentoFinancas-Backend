170f4d3dd7f9087cf43c01bc11dcd6dad32b724b
felipeErnica <felipe.ernica@gmail.com>
2025-01-15 10:32:37 -0300

    Refatoracao BancoController, BancoService, BancoDTO

--- src/main/java/com/santacarolina/financeiro/service/BancoService.java
+++ src/main/java/com/santacarolina/financeiro/service/BancoService.java
@@ -5,6 +5,7 @@ import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.dao.OptimisticLockingFailureException;
+import org.springframework.stereotype.Service;
 
 import com.santacarolina.financeiro.dto.BancoDTO;
 import com.santacarolina.financeiro.entity.BancoEntity;
@@ -13,6 +14,7 @@ import com.santacarolina.financeiro.repository.BancoRepository;
 /**
  * BancoService
  */
+@Service
 public class BancoService {
 
     @Autowired
