9fd4361e98740c42827cf547e32dd1927cef6a8f
felipeErnica <felipe.ernica@gmail.com>
2025-01-16 15:55:02 -0300

    Refatoracao JPA de Produto

--- src/main/java/com/santacarolina/financeiro/repository/ProdutoRepository.java
+++ src/main/java/com/santacarolina/financeiro/repository/ProdutoRepository.java
@@ -2,8 +2,8 @@ package com.santacarolina.financeiro.repository;
 
 import java.util.List;
 
-import org.springframework.data.jdbc.repository.query.Query;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
 
 import com.santacarolina.financeiro.entity.ProdutoEntity;
