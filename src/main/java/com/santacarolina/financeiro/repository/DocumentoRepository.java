67cdd0c8b8d68e28b8e5f0dd3c90d08882b1d390
felipeErnica <felipe.ernica@gmail.com>
2025-01-16 11:36:54 -0300

    Refatoracao JPA de Documento

--- src/main/java/com/santacarolina/financeiro/repository/DocumentoRepository.java
+++ src/main/java/com/santacarolina/financeiro/repository/DocumentoRepository.java
@@ -3,8 +3,8 @@ package com.santacarolina.financeiro.repository;
 import java.time.LocalDate;
 import java.util.Optional;
 
-import org.springframework.data.jdbc.repository.query.Query;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
 
 import com.santacarolina.financeiro.entity.DocumentoEntity;
