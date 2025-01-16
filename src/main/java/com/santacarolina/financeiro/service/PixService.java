31c263eb02eda03e7080b53effeef3a4d5b6c8ec
felipeErnica <felipe.ernica@gmail.com>
2025-01-16 16:43:11 -0300

    Refatoracao JPA de Pix

--- src/main/java/com/santacarolina/financeiro/service/PixService.java
+++ src/main/java/com/santacarolina/financeiro/service/PixService.java
@@ -5,6 +5,7 @@ import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.dao.OptimisticLockingFailureException;
+import org.springframework.stereotype.Service;
 
 import com.santacarolina.financeiro.dto.PixDTO;
 import com.santacarolina.financeiro.entity.PixEntity;
@@ -13,6 +14,7 @@ import com.santacarolina.financeiro.repository.PixRepository;
 /**
  * PixService
  */
+@Service
 public class PixService {
 
     @Autowired
