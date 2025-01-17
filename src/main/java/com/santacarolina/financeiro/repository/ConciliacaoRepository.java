502952e815273f646c67a3af81e3f05120f9a937
felipeErnica <felipe.ernica@gmail.com>
2025-01-17 11:24:29 -0300

    Queries JPA para otmizacao do repositorio Conciliacao

--- src/main/java/com/santacarolina/financeiro/repository/ConciliacaoRepository.java
+++ src/main/java/com/santacarolina/financeiro/repository/ConciliacaoRepository.java
@@ -1,6 +1,9 @@
 package com.santacarolina.financeiro.repository;
 
+import java.util.List;
+
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
 
 import com.santacarolina.financeiro.entity.ConciliacaoEntity;
@@ -10,4 +13,14 @@ import com.santacarolina.financeiro.entity.ConciliacaoEntity;
  */
 @Repository
 public interface ConciliacaoRepository extends JpaRepository<ConciliacaoEntity, Long>{
+    
+    @Override
+    @Query("""
+        SELECT c, d, e
+        FROM ConciliacaoEntity c
+        LEFT JOIN ExtratoEntity e ON c.extrato.id = e.id
+        LEFT JOIN DuplicataEntity d ON d.id = c.duplicata.id
+        ORDER BY e.dataTransacao DESC
+        """)
+    List<ConciliacaoEntity> findAll();
 }
