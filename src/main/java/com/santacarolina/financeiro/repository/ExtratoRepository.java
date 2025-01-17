f9a730f834f0a3fa5d2eaaf6cd1e14f08049aec0
felipeErnica <felipe.ernica@gmail.com>
2025-01-17 19:13:31 -0300

    Adiciona novas queries no Repositorio de Extrato

--- src/main/java/com/santacarolina/financeiro/repository/ExtratoRepository.java
+++ src/main/java/com/santacarolina/financeiro/repository/ExtratoRepository.java
@@ -15,11 +15,22 @@ import com.santacarolina.financeiro.entity.ExtratoEntity;
 public interface ExtratoRepository extends JpaRepository<ExtratoEntity, Long> {
 
     @Query("""
-        SELECT e
+        SELECT e, c, b
         FROM ExtratoEntity e
+        LEFT JOIN ContaEntity c ON c.id = e.conta.id
+        LEFT JOIN BancoEntity b ON b.id = c.banco.id
         WHERE e.conta.id = :contaId
+        ORDER BY e.dataTransacao DESC
     """)
     List<ExtratoEntity> findByContaId(long contaId);
 
+    @Query("""
+        SELECT e, c, b
+        FROM ExtratoEntity e
+        LEFT JOIN ContaEntity c ON c.id = e.conta.id
+        LEFT JOIN BancoEntity b ON b.id = c.banco.id
+        WHERE e.conciliado = :isConciliado
+        ORDER BY e.dataTransacao DESC
+    """)
     List<ExtratoEntity> findByConciliado(boolean isConciliado);
 }
