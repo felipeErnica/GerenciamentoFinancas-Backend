6bedef42d1c639b727b222f8830844cbc17f5cd5
felipeErnica <felipe.ernica@gmail.com>
2025-01-17 11:30:00 -0300

    Queries JPA para otmizacao do repositorio Conta

--- src/main/java/com/santacarolina/financeiro/repository/ContaRepository.java
+++ src/main/java/com/santacarolina/financeiro/repository/ContaRepository.java
@@ -1,5 +1,6 @@
 package com.santacarolina.financeiro.repository;
 
+import java.util.List;
 import java.util.Optional;
 
 import org.springframework.data.jpa.repository.JpaRepository;
@@ -15,9 +16,29 @@ import com.santacarolina.financeiro.entity.ContaEntity;
 @Repository
 public interface ContaRepository extends JpaRepository<ContaEntity, Long> {
 
+    @Override
     @Query("""
-        SELECT c 
+        SELECT c, b 
         FROM ContaEntity c
+        LEFT JOIN BancoEntity b ON b.id = c.banco.id
+        ORDER BY c.nomeConta
+        """)
+    List<ContaEntity> findAll();
+
+
+    @Override
+    @Query("""
+        SELECT c, b 
+        FROM ContaEntity c
+        LEFT JOIN BancoEntity b ON b.id = c.banco.id
+        WHERE c.id = :id
+        """)
+    Optional<ContaEntity> findById(Long id);
+
+    @Query("""
+        SELECT c, b 
+        FROM ContaEntity c
+        LEFT JOIN BancoEntity b ON b.id = c.banco.id
         WHERE c.agencia = :agencia AND c.numeroConta = :numeroConta AND c.banco.id = :bancoId
         """)
     Optional<ContaEntity> findEqual(@Param("agencia") String agencia, 
