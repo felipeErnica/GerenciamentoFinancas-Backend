8f03ec6033fde30ff11134bc5d7dffb3981a53bf
felipeErnica <felipe.ernica@gmail.com>
2025-01-16 21:19:47 -0300

    Refatoracao JPA de Produtos+Duplicatas

--- src/main/java/com/santacarolina/financeiro/service/ProdutoDuplicataService.java
+++ src/main/java/com/santacarolina/financeiro/service/ProdutoDuplicataService.java
@@ -1,6 +1,5 @@
 package com.santacarolina.financeiro.service;
 
-import java.util.ArrayList;
 import java.util.Collections;
 import java.util.List;
 import java.util.Map;
@@ -13,11 +12,6 @@ import org.springframework.stereotype.Service;
 import com.santacarolina.financeiro.dto.DuplicataDTO;
 import com.santacarolina.financeiro.dto.ProdutoDTO;
 import com.santacarolina.financeiro.dto.ProdutoDuplicataDTO;
-import com.santacarolina.financeiro.entity.DocumentoEntity;
-import com.santacarolina.financeiro.entity.DuplicataEntity;
-import com.santacarolina.financeiro.entity.ProdutoEntity;
-import com.santacarolina.financeiro.repository.DocumentoRepository;
-import com.santacarolina.financeiro.repository.DuplicataRepository;
 import com.santacarolina.financeiro.repository.ProdutoRepository;
 
 /**
@@ -26,66 +20,11 @@ import com.santacarolina.financeiro.repository.ProdutoRepository;
 @Service
 public class ProdutoDuplicataService {
 
-    private ProdutoRepository produtoRepository;
-    private DuplicataRepository duplicataRepository;
-    private DocumentoRepository documentoRepository;
-
     @Autowired
-    public ProdutoDuplicataService(
-        ProdutoRepository produtoRepository, 
-        DuplicataRepository duplicataRepository,
-        DocumentoRepository documentoRepository) {
-
-        this.produtoRepository = produtoRepository;
-        this.duplicataRepository = duplicataRepository;
-        this.documentoRepository = documentoRepository;
-    }
-
-    private void addElements(List<ProdutoEntity> listProdutos, 
-            List<DuplicataEntity> listDuplicatas,
-            List<ProdutoDuplicataDTO> produtoDuplicataList) {
-
-        for (ProdutoEntity produto : listProdutos) {
-            for (DuplicataEntity duplicata : listDuplicatas) {
-                ProdutoDuplicataDTO produtoDuplicataDTO = new ProdutoDuplicataDTO(produto, duplicata);
-                produtoDuplicataList.add(produtoDuplicataDTO);
-            }
-        }
-    }
-
-    private List<ProdutoDuplicataDTO> buildList(Map<Long, List<ProdutoEntity>> filteredProd,
-            Map<Long, List<DuplicataEntity>> filteredDups, 
-            List<DocumentoEntity> documentoEntities) {
-
-        List<ProdutoDuplicataDTO> produtoDuplicataList = new ArrayList<>();
-        for (DocumentoEntity documento : documentoEntities) {
-            long documentoId = documento.getId();
-            List<ProdutoEntity> listProdutos = filteredProd.getOrDefault(documentoId, Collections.emptyList());
-            List<DuplicataEntity> listDuplicatas = filteredDups.getOrDefault(documentoId, Collections.emptyList());
-            int numDup = listDuplicatas.size();
-            listProdutos.forEach(prod -> prod.setQuantidade(prod.getQuantidade()/numDup));
-            addElements(listProdutos, listDuplicatas, produtoDuplicataList);
-        }
-
-        return produtoDuplicataList;
-    }
+    private ProdutoRepository repository;
 
     public List<ProdutoDuplicataDTO> findProdutosDuplicatas() {
-        List<ProdutoEntity> produtoEntities = produtoRepository.findAll();
-        List<DuplicataEntity> duplicataEntities = duplicataRepository.findAll();
-        List<DocumentoEntity> documentoEntities = documentoRepository.findAll();
-        
-        Map<Long, List<ProdutoEntity>> filteredProd = produtoEntities.stream()
-            .collect(Collectors.groupingBy(prod -> prod.getDocumento().getId()));
-        Map<Long, List<DuplicataEntity>> filteredDups = duplicataEntities.stream()
-            .collect(Collectors.groupingBy(dup -> dup.getDocumento().getId()));
-
-        return buildList(filteredProd, filteredDups, documentoEntities);
-
-    }
-
-    public List<ProdutoDuplicataDTO> findTest() {
-        List<ProdutoDuplicataDTO> produtoDuplicataList = produtoRepository.findProdutosDuplicatas();
+        List<ProdutoDuplicataDTO> produtoDuplicataList = repository.findProdutosDuplicatas();
         
         Map<Long, List<DuplicataDTO>> duplicataMap = produtoDuplicataList.stream()
             .map(prodDup -> prodDup.getDuplicata())
@@ -105,7 +44,6 @@ public class ProdutoDuplicataService {
         }
 
         return produtoDuplicataList;
-
     }
 
 }
