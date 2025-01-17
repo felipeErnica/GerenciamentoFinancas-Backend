30192b7a92c1871f6dad76ad74fb8bb178e9b477
felipeErnica <felipe.ernica@gmail.com>
2025-01-17 10:31:47 -0300

    Teste de informacoes aninhadas

--- src/main/java/com/santacarolina/financeiro/repository/ClassificacaoRepository.java
+++ src/main/java/com/santacarolina/financeiro/repository/ClassificacaoRepository.java
@@ -1,8 +1,10 @@
 package com.santacarolina.financeiro.repository;
 
+import java.util.List;
 import java.util.Optional;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
 
 import com.santacarolina.financeiro.entity.ClassificacaoEntity;
@@ -12,6 +14,18 @@ import com.santacarolina.financeiro.entity.ClassificacaoEntity;
  */
 @Repository
 public interface ClassificacaoRepository extends JpaRepository<ClassificacaoEntity, Long> {
+
+    @Query(value = """
+        SELECT class.*, cat.*
+        FROM classificacoes_contabeis class
+        LEFT JOIN categorias_contabeis cat ON cat.id = class.categoria_id
+        ORDER BY class.numero_identificacao;
+        """, nativeQuery = true)
+    List<ClassificacaoEntity> findAll();
+    
+    Optional<ClassificacaoEntity> findById(long id);
+    
     Optional<ClassificacaoEntity> findByNumeroIdentificacao(String numeroIdentificacao);
+    
     Optional<ClassificacaoEntity> findByNomeClassificacao(String nomeClassificacao);
 }
