49ca13b87ae993d74f3d93dfcab8ccc5134c1f00
felipeErnica <felipe.ernica@gmail.com>
2025-01-17 14:25:26 -0300

    Query customizadas em Repositorio Dado

--- src/main/java/com/santacarolina/financeiro/repository/DadoRepository.java
+++ src/main/java/com/santacarolina/financeiro/repository/DadoRepository.java
@@ -12,9 +12,21 @@ import com.santacarolina.financeiro.entity.DadoEntity;
 @Repository
 public interface DadoRepository extends JpaRepository<DadoEntity, Long> {
 
+    @Override
+    @Query("""
+        SELECT d, b, c
+        FROM DadoEntity d
+        LEFT JOIN BancoEntity b ON b.id = d.banco.id
+        LEFT JOIN ContatoEntity c ON c.id = d.contato.id
+        ORDER BY c.nome
+        """)
+    List<DadoEntity> findAll();
+
     @Query("""
         SELECT d
         FROM DadoEntity d
+        LEFT JOIN BancoEntity b ON b.id = d.banco.id
+        LEFT JOIN ContatoEntity c ON c.id = d.contato.id
         WHERE d.contato.id = :contatoId
         """)
     List<DadoEntity> findByContato(long contatoId);
@@ -22,7 +34,20 @@ public interface DadoRepository extends JpaRepository<DadoEntity, Long> {
     @Query("""
         SELECT d
         FROM DadoEntity d
+        LEFT JOIN BancoEntity b ON b.id = d.banco.id
+        LEFT JOIN ContatoEntity c ON c.id = d.contato.id
         WHERE d.agencia = :agencia AND d.numeroConta = :numeroConta AND d.banco.id = :bancoId
         """)
     Optional<DadoEntity> findEqual(String agencia, String numeroConta, long bancoId);
+
+    @Override
+    @Query("""
+        SELECT d
+        FROM DadoEntity d
+        LEFT JOIN BancoEntity b ON b.id = d.banco.id
+        LEFT JOIN ContatoEntity c ON c.id = d.contato.id
+        WHERE d.agencia = :agencia AND d.numeroConta = :numeroConta AND d.banco.id = :bancoId
+        """)
+    Optional<DadoEntity> findById(Long id);
+
 }
