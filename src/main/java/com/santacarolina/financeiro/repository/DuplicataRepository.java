bee6bbd0d6ebf7a19def22dc7066f74700b8870b
felipeErnica <felipe.ernica@gmail.com>
2025-01-17 17:59:19 -0300

    Query customizadas em Repositorio Duplicatas

--- src/main/java/com/santacarolina/financeiro/repository/DuplicataRepository.java
+++ src/main/java/com/santacarolina/financeiro/repository/DuplicataRepository.java
@@ -17,42 +17,42 @@ public interface DuplicataRepository extends JpaRepository<DuplicataEntity, Long
     
     @Override
     @Query("""
-        SELECT dup, doc, conta, p
+        SELECT dup, doc, dado, pix
         FROM DuplicataEntity dup
         LEFT JOIN DocumentoEntity doc ON doc.id = dup.documento.id
-        LEFT JOIN DadoEntity conta ON conta.id = doc.dado.id
-        LEFT JOIN PixEntity p ON p.id = doc.pix.id
+        LEFT JOIN DadoEntity dado ON dado.id = dup.dado.id
+        LEFT JOIN PixEntity pix ON pix.id = dup.pix.id
         ORDER BY dup.dataVencimento DESC
         """) 
     List<DuplicataEntity> findAll();
 
     @Override
     @Query("""
-        SELECT dup, doc, conta, p
+        SELECT dup, doc, dado, pix
         FROM DuplicataEntity dup
         LEFT JOIN DocumentoEntity doc ON doc.id = dup.documento.id
-        LEFT JOIN DadoEntity conta ON conta.id = doc.dado.id
-        LEFT JOIN PixEntity p ON p.id = doc.pix.id
+        LEFT JOIN DadoEntity dado ON dado.id = dup.dado.id
+        LEFT JOIN PixEntity pix ON pix.id = dup.pix.id
         WHERE dup.id = :id
         """) 
     Optional<DuplicataEntity> findById(Long id);
 
     @Query("""
-        SELECT dup, doc, conta, p
+        SELECT dup, doc, dado, pix
         FROM DuplicataEntity dup
         LEFT JOIN DocumentoEntity doc ON doc.id = dup.documento.id
-        LEFT JOIN DadoEntity conta ON conta.id = doc.dado.id
-        LEFT JOIN PixEntity p ON p.id = doc.pix.id
+        LEFT JOIN DadoEntity dado ON dado.id = dup.dado.id
+        LEFT JOIN PixEntity pix ON pix.id = dup.pix.id
         WHERE dup.paga = :paga
         """) 
     List<DuplicataEntity> findByPaga(boolean paga);
 
     @Query("""
-        SELECT dup, doc, conta, p
+        SELECT dup, doc, dado, pix
         FROM DuplicataEntity dup
         LEFT JOIN DocumentoEntity doc ON doc.id = dup.documento.id
-        LEFT JOIN DadoEntity conta ON conta.id = doc.dado.id
-        LEFT JOIN PixEntity p ON p.id = doc.pix.id
+        LEFT JOIN DadoEntity dado ON dado.id = dup.dado.id
+        LEFT JOIN PixEntity pix ON pix.id = dup.pix.id
         WHERE d.documento.id = :documentoId
         ORDER BY dup.dataVencimento
         """)
