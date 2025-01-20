c4a6687301476b70729c3585c9ce1aae00e3f3ae
felipeErnica <felipe.ernica@gmail.com>
2025-01-20 18:21:24 -0300

    Correcao PastaDTO para incluir Conta

--- src/main/java/com/santacarolina/financeiro/dto/PastaDTO.java
+++ src/main/java/com/santacarolina/financeiro/dto/PastaDTO.java
@@ -7,18 +7,18 @@ public class PastaDTO {
     private long id;
     private String nome;
     private String caminhoPasta;
-    private long contaId;
+    private ContaDTO conta;
 
     public PastaDTO(PastaEntity entity) {
         this.id = entity.getId();
         this.nome = entity.getNome();
         this.caminhoPasta = entity.getCaminhoPasta();
-        this.contaId = entity.getConta().getId();
+        this.conta = entity.getConta() != null ? new ContaDTO(entity.getConta()) : null;
     }
 
     public long getId() { return id; }
     public String getNome() { return nome; }
     public String getCaminhoPasta() { return caminhoPasta; }
-    public long getContaId() { return contaId; }
+    public ContaDTO getConta() { return conta; }
 
 }
