9d6d3d5cc5c139da66ab332e8c9545e82e768145
felipeErnica <felipe.ernica@gmail.com>
2025-01-23 14:44:41 -0300

    Adiciona ordenamento ao método findAll em CategoriaRepository

--- src/main/java/com/santacarolina/financeiro/repository/CategoriaRepository.java
+++ src/main/java/com/santacarolina/financeiro/repository/CategoriaRepository.java
@@ -1,7 +1,9 @@
 package com.santacarolina.financeiro.repository;
 
+import java.util.List;
 import java.util.Optional;
 
+import org.springframework.data.jdbc.repository.query.Query;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
@@ -12,6 +14,11 @@ import com.santacarolina.financeiro.entity.CategoriaEntity;
  */
 @Repository
 public interface CategoriaRepository extends JpaRepository<CategoriaEntity, Long> {
+
+    @Override
+    @Query("SELECT pasta FROM PastaEntity pasta ORDER BY pasta.numeroCategoria")
+    List<CategoriaEntity> findAll();
+
     Optional<CategoriaEntity> findByNome(String nome);
     Optional<CategoriaEntity> findByNumeroCategoria(String numeroCategoria);
 }
