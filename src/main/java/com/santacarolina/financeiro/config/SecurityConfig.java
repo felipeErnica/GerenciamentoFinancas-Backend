b3c81c08be2ffebe10a16f35a5c6a0e16949edb8
felipeErnica <felipe.ernica@gmail.com>
2025-01-29 15:29:47 -0300

    Mudança na configuração de segurança

--- src/main/java/com/santacarolina/financeiro/config/SecurityConfig.java
+++ src/main/java/com/santacarolina/financeiro/config/SecurityConfig.java
@@ -16,7 +16,7 @@ public class SecurityConfig {
     public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
         http
             .authorizeHttpRequests((auth) -> auth
-                .requestMatchers("/user/**").permitAll()
+                .requestMatchers("/user/*").permitAll()
                 .anyRequest().authenticated()
             );
         return http.build();
