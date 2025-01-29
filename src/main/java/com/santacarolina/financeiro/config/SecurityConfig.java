046c10a2711aff867e5810b795807b76219eba08
felipeErnica <felipe.ernica@gmail.com>
2025-01-29 12:34:47 -0300

    Autenticação e registro de usuários

--- src/main/java/com/santacarolina/financeiro/config/SecurityConfig.java
+++ src/main/java/com/santacarolina/financeiro/config/SecurityConfig.java
@@ -4,6 +4,8 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.web.server.ServerHttpSecurity;
+import org.springframework.security.crypto.factory.PasswordEncoderFactories;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.server.SecurityWebFilterChain;
 
 @Configuration
@@ -14,6 +16,11 @@ public class SecurityConfig {
     public SecurityWebFilterChain securityFilterChain(ServerHttpSecurity http) throws Exception {
         http.authorizeExchange((authorize) -> authorize.pathMatchers("/user/**").permitAll());
         return http.build();
+    }
+
+    @Bean
+    public PasswordEncoder passwordEncoder() {
+        return PasswordEncoderFactories.createDelegatingPasswordEncoder();
     }
 
 }
