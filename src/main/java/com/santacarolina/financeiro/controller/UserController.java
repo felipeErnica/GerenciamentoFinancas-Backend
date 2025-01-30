17a82e02869af50478520a5e0b8f3debb45bec32
felipeErnica <felipe.ernica@gmail.com>
2025-01-30 11:56:03 -0300

    Debug da autenticação

--- src/main/java/com/santacarolina/financeiro/controller/UserController.java
+++ src/main/java/com/santacarolina/financeiro/controller/UserController.java
@@ -4,17 +4,17 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.core.context.SecurityContextHolder;
-import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 import com.santacarolina.financeiro.dto.LoginDTO;
 import com.santacarolina.financeiro.entity.AuthToken;
 import com.santacarolina.financeiro.entity.UserEntity;
 import com.santacarolina.financeiro.service.UserService;
 
-@Controller
+@RestController
 @RequestMapping("/user")
 public class UserController {
 
