b23032b7b97148839a0fab6661fc8704b8de89b2
felipeErnica <felipe.ernica@gmail.com>
2025-01-30 09:46:43 -0300

    Mudança na autenticação

--- src/main/java/com/santacarolina/financeiro/config/SecurityConfig.java
+++ src/main/java/com/santacarolina/financeiro/config/SecurityConfig.java
@@ -1,10 +1,13 @@
 package com.santacarolina.financeiro.config;
 
+import java.util.List;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.authentication.AuthenticationManager;
-import org.springframework.security.config.annotation.authentication.configuration.AuthenticationConfiguration;
+import org.springframework.security.authentication.AuthenticationProvider;
+import org.springframework.security.authentication.ProviderManager;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.http.SessionCreationPolicy;
@@ -41,13 +44,11 @@ public class SecurityConfig {
     }
 
     @Bean
-    public PasswordEncoder passwordEncoder() {
-        return PasswordEncoderFactories.createDelegatingPasswordEncoder();
-    }
+    public PasswordEncoder passwordEncoder() { return PasswordEncoderFactories.createDelegatingPasswordEncoder(); }
 
     @Bean
-    public AuthenticationManager authenticationManager (AuthenticationConfiguration authenticationConfiguration) throws Exception {
-        return authenticationConfiguration.getAuthenticationManager();
+    public AuthenticationManager authenticationManager(List<AuthenticationProvider> myAuthenticationProviders) {
+        return new ProviderManager(myAuthenticationProviders);
     }
 
 }
