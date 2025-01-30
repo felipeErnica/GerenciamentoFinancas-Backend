9f0c630ae85081e18b4abe5a512a54362899e501
felipeErnica <felipe.ernica@gmail.com>
2025-01-30 14:22:38 -0300

    Autenticação bem sucedida! - Testes de configuração

--- src/main/java/com/santacarolina/financeiro/controller/UserController.java
+++ src/main/java/com/santacarolina/financeiro/controller/UserController.java
@@ -3,7 +3,6 @@ package com.santacarolina.financeiro.controller;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -24,7 +23,6 @@ public class UserController {
     @PostMapping("/login")
     public ResponseEntity<AuthToken> authenticate(@RequestBody LoginDTO login) {
         AuthToken token = service.authenticateUser(login);
-        System.out.println((String) SecurityContextHolder.getContext().getAuthentication().getPrincipal());
         if (token.token() == null) return new ResponseEntity<>(HttpStatus.NOT_ACCEPTABLE);
         return ResponseEntity.ok(token);
     }
