40c2c740a9036c3c0ac342fb795573c2b4ed924b
felipeErnica <felipe.ernica@gmail.com>
2025-01-30 11:51:57 -0300

    Debug da autenticação

--- src/main/java/com/santacarolina/financeiro/service/CustomUserDetailsService.java
+++ src/main/java/com/santacarolina/financeiro/service/CustomUserDetailsService.java
@@ -4,10 +4,12 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
 
 import com.santacarolina.financeiro.entity.UserEntity;
 import com.santacarolina.financeiro.repository.UserRepository;
 
+@Service
 public class CustomUserDetailsService implements UserDetailsService {
 
     @Autowired
