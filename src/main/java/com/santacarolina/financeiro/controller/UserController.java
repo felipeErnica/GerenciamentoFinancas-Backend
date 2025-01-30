962a8df0d179b1310a2a24fd7c1ac91d6cdcfdb3
felipeErnica <felipe.ernica@gmail.com>
2025-01-30 11:35:36 -0300

    Debug da autenticação

--- src/main/java/com/santacarolina/financeiro/controller/UserController.java
+++ src/main/java/com/santacarolina/financeiro/controller/UserController.java
@@ -9,6 +9,7 @@ import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 
+import com.santacarolina.financeiro.dto.LoginDTO;
 import com.santacarolina.financeiro.entity.AuthToken;
 import com.santacarolina.financeiro.entity.UserEntity;
 import com.santacarolina.financeiro.service.UserService;
@@ -21,8 +22,8 @@ public class UserController {
     private UserService service;
 
     @PostMapping("/login")
-    public ResponseEntity<AuthToken> authenticate(@RequestBody UserEntity user) {
-        AuthToken token = service.authenticateUser(user);
+    public ResponseEntity<AuthToken> authenticate(@RequestBody LoginDTO login) {
+        AuthToken token = service.authenticateUser(login);
         UserEntity authUser = (UserEntity) SecurityContextHolder.getContext().getAuthentication().getPrincipal();
         System.out.println("ID usuário: " + authUser.getId());
         if (token.token() == null) return new ResponseEntity<>(HttpStatus.NOT_ACCEPTABLE);
