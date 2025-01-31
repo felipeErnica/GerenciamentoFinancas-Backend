f95ec0379421e7a490a59ae7f94f3de396bf10ce
felipeErnica <felipe.ernica@gmail.com>
2025-01-31 09:14:19 -0300

    Muda a lógica de verificação da URL

--- src/main/java/com/santacarolina/financeiro/controller/UserController.java
+++ src/main/java/com/santacarolina/financeiro/controller/UserController.java
@@ -4,6 +4,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -22,7 +23,7 @@ public class UserController {
     private UserService service;
 
     @GetMapping("/username/{username}")
-    public ResponseEntity<UserEntity> findByUsername(String username) {
+    public ResponseEntity<UserEntity> findByUsername(@PathVariable String username) {
         return service.findByUsername(username)
             .map(user -> ResponseEntity.ok(user))
             .orElseGet(() -> ResponseEntity.notFound().build());
--- src/main/java/com/santacarolina/financeiro/util/UserAuthenticationFilter.java
+++ src/main/java/com/santacarolina/financeiro/util/UserAuthenticationFilter.java
@@ -56,9 +56,8 @@ public class UserAuthenticationFilter extends OncePerRequestFilter {
     // Verifica se o endpoint requer autenticação antes de processar a requisição
     private boolean checkIfEndpointIsNotPublic(HttpServletRequest request) {
         String requestURI = request.getRequestURI();
-        System.out.println("\n URL: " + requestURI + "\n");
         for (String uri : SecurityConfig.ENDPOINTS_WITH_AUTHENTICATION_NOT_REQUIRED) {
-            if (uri.equals(requestURI)) return false;
+            if (uri.contains(requestURI)) return false;
         }
         return true;
     }
